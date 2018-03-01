10b684140ee809c99a2de871fc5483410bc4da42
S3baRr00 <36234060+S3baRr00@users.noreply.github.com>
2018-03-01 12:21:27 -0300

    Add files via upload

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -6,15 +6,51 @@ y una de las cuatro operaciones básicas (suma, resta,
  el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
-var respuesta;
 function comenzar()
 {
+var num1= (Math.random()*10).toFixed(0);
+document.getElementById("PrimerNumero").value=num1;
+var num2= (Math.random()*10).toFixed(0);	
+document.getElementById("SegundoNumero").value=num2;
+var operador= (Math.random()*3).toFixed(0);
+var resultado;
 
-	
 
+switch (operador)
+{
+    case '0':
+    {
+        document.getElementById("Operador").value="+";
+        resultado= num1+num2;
+        break;
+    }
+    case '1':
+    {
+        document.getElementById("Operador").value="-";
+        resultado= num1-num2;
+        break;
+    }
+    case '2':
+    {
+        document.getElementById("Operador").value="X";
+        resultado= num1*num2;
+        break;
+    }
+    case '3':
+    {
+        document.getElementById("Operador").value="/";
+        resultado.toFixed(2)= num1 / num2;
+        break;
+    }
+
+
+
+
+}    
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    var respuesta= 
+    
 
 }//FIN DE LA FUNCIÓN
